257fa4b096b3e0508b741138b846ce0e21a459d4
rohan2oo3 <rohandutta736@gmail.com>
2024-05-30 10:33:12 +0530

    Add files via upload

---
+++ DriverUrlMapper.kt
@@ -0,0 +1,36 @@
+package com.cabapp.cabappspringboot.driver
+import com.cabapp.cabappspringboot.driver.service.S3Service
+import com.cabapp.cabappspringboot.shared.Shared.pol1
+import com.cabapp.cabappspringboot.shared.Shared.pol2
+import com.cabapp.cabappspringboot.shared.Shared.pos1
+import com.cabapp.cabappspringboot.shared.Shared.pos2
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+
+
+@RestController
+@RequestMapping("driver/")
+class DriverUrlMapper {
+    @Autowired
+    private val s3Service: S3Service? = null
+
+    @Autowired
+    lateinit var driverAccountDetailsObj: DriverAccountDetails
+
+    @CrossOrigin(origins = [pol1,pol2,pos1,pos2])
+    @PostMapping("/update-details/")
+    fun updateDetails(@RequestBody payload: String?, header: HttpServletRequest) = payload?.let {driverAccountDetailsObj.updateDetails(it, header)}
+
+    @CrossOrigin(origins = [pol1,pol2,pos1,pos2])
+    @PostMapping("view-details/")
+    fun viewDetails(@RequestBody payload: String?, header: HttpServletRequest) = payload?.let {driverAccountDetailsObj.viewDetails(it, header)}
+
+    @CrossOrigin(origins = [pol1, pol2, pos1, pos2])
+    @PostMapping("/upload")
+    fun uploadRC(@RequestParam("file") file: MultipartFile?): String? {
+        return s3Service!!.saveFile(file)
+    }
+
+}
---
+++ service/FileServiceImpl.kt
@@ -0,0 +1,15 @@
+package com.cabapp.cabappspringboot.driver.service
+
+import org.springframework.web.multipart.MultipartFile
+
+interface FileServiceImpl {
+    fun saveFile(file: MultipartFile?): String?
+
+    fun downloadFile(filename: String?): ByteArray?
+
+
+    fun deleteFile(filename: String?): String?
+
+
+    fun listAllFiles(): List<String?>?
+}
